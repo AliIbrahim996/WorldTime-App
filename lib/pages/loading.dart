9a8ceded9277c8a8de0d074d39dfb8af25418cf0
ALi Ibrahim <allosheribraheem38@gmail.com>
2020-10-12 20:06:50 +0300

    add spinKit

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:time_app/services/worldTime.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -13,9 +14,9 @@ class _LoadingState extends State<Loading> {
           location: 'Syria', avatar: 'syria.png', urlEndPoint: 'Asia/Damascus');
       await t.calculateTime();
       Navigator.pushReplacementNamed(context, '/home', arguments: {
-        'location' : t.location,
-        'flag':t.avatar,
-        'time':t.getTime()
+        'location': t.location,
+        'flag': t.avatar,
+        'time': t.getTime()
       });
     } catch (e) {
       print('Error: $e');
@@ -31,11 +32,15 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[400],
+      backgroundColor: Colors.greenAccent[700],
       body: SafeArea(
           child: Padding(
         padding: const EdgeInsets.all(12.0),
-        child: Text('Loading'),
+        child: Center(
+            child: SpinKitCircle(
+          color: Colors.yellowAccent,
+          size: 50.0,
+        )),
       )),
     );
   }
