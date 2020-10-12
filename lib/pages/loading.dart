73b688bb14fb8e749450db9d3cd4a451dadca4f3
ALi Ibrahim <allosheribraheem38@gmail.com>
2020-10-12 18:31:22 +0300

    finished building worldTime class

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'dart:convert';
+import 'package:time_app/services/worldTime.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -8,23 +7,24 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  void getData() async {
-    Response response =
-        await get('https://jsonplaceholder.typicode.com/todos/1');
-    Map data = jsonDecode(response.body);
-    print(data);
+  void setUpTime() async {
+    WorldTime t = WorldTime(
+        location: 'Syria', avatar: 'syria.png', urlEndPoint: 'Asia/Damascus');
+    await t.calculateTime();
+    print(t.getTime());
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    setUpTime();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[400],
+      body: Text('Loading Screen'),
     );
   }
 }
--- lib/services/worldTime.dart
+++ lib/services/worldTime.dart
@@ -1,6 +1,35 @@
-import 'package:flutter/material.dart';
 import 'dart:convert';
+import 'package:http/http.dart';
 
-class _worldTime{
-  
+class WorldTime {
+  String location; // Location name for UI
+  String _Time; // The Time in Location
+  String avatar; //url to an asset flag icon of a location
+  String urlEndPoint; //location url for api end point
+
+  WorldTime({this.location, this.avatar, this.urlEndPoint});
+  void setTime(String time) {
+    this._Time = time;
+  }
+
+  String getTime() {
+    return this._Time;
+  }
+
+  Future<void> calculateTime() async {
+    //make request
+    Response response =
+        await get('http://worldtimeapi.org/api/timezone/$urlEndPoint');
+    Map data = jsonDecode(response.body);
+    print(data);
+
+    //get properties from data
+    String dateTime = data['datetime'];
+    String offset = data['utc_offset'].substring(1, 3);
+
+    //Create DateTime object
+    DateTime date = DateTime.parse(dateTime);
+    date.add(Duration(hours: int.parse(offset)));
+    setTime(date.toString());
+  }
 }
